8f9f43aa983406b663709fb05f10a8ef7d4e960e
Your Namenayandeep-09 <nayandeepchouhan864@gmail.com>
2025-12-27 00:36:09 +0530

    updated_switch_statment

--- if_else_statements.c
+++ if_else_statements.c
@@ -87,3 +87,44 @@ int main(){
 /*RELATIONAL OPERATORS IN C
 ==, >=, >, <, <=, !=
 */
+
+
+#include <stdio.h>
+
+int main(){
+    int m;
+
+    printf("Your marks: ");
+    scanf("%d", &m);
+
+    if (m >= 90 && m <= 100)
+    {
+        printf("Grade A");
+    }
+    else if (m>=80 && m<=90)
+    {
+        printf("Grade B");
+    }else if (m>=70 && m<=80)
+    {
+        printf("Grade C");
+    }else if (m>=60 && m<=70)
+    {
+        printf("Grade D");
+    }else if (m>=50 && m<=60)
+    {
+        printf("Grade E");
+    }else if (m<=49)
+    {
+        printf("Failed");
+    }
+    else{
+        printf("Error");
+    }
+    
+    return 0;
+}
+
+/*
+Your marks: 50
+Grade E
+*/
